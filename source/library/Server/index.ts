37f1963eb75270fa19b2ef0b29e89483bdb0bc23
Алексей Чепугов <a.chepugov@gmail.com>
2020-07-13 00:13:28 +0300

    chore (Server) export

--- source/library/Server/index.ts
+++ source/library/Server/index.ts
@@ -1,5 +1,5 @@
 export {Server} from './Server';
-export {Server as default} from './Server';
+export {default as default} from './Server';
 export {Router} from './Router';
 export {Context} from './library/Context';
 export {Method} from './library/Method';
