a308983599ca125bfa66fcc831a0c2454f154c86
Алексей Чепугов <a.chepugov@gmail.com>
2020-07-20 11:40:00 +0300

    refactor(Server)

--- source/library/Server/Server/index.ts
+++ source/library/Server/Server/index.ts
@@ -1,51 +1,20 @@
 import * as http from 'http';
 
-import {Listener, Context, ContextListener} from "./Listener";
+import {RequestListener, Listener, Context, ContextListener} from "./Listener";
 import {IncomingMessage, ServerResponse} from "http";
 
 export {Context, ContextListener} from "./Listener";
 
 class ServerCore {
-	protected readonly _listener: Listener;
+	protected readonly _listener: RequestListener;
 	protected readonly _server: http.Server;
-
-	constructor(listener: Listener) {
-		Object.defineProperty(this, '_listener', {value: listener});
-		Object.defineProperty(this, '_server', {value: http.createServer(this._listener.listen)});
-	}
-
-	interceptor = (interceptor: (ctx: Context, error: any) => void) => {
-		this._listener.interceptor(interceptor);
-		return this;
-	}
-
-	with = (setter: (request: IncomingMessage, response: ServerResponse) => any) => {
-		this._listener.with(setter);
-		return this;
-	};
-
-	use = (nextRequestListener: ContextListener) => {
-		this._listener.register(nextRequestListener);
-		return this;
-	}
-
-	unuse = (nextRequestListener: ContextListener) => {
-		this._listener.unregister(nextRequestListener);
-		return this;
-	}
-
-	get server() {
-		return this._server;
-	}
-}
-
-export class Server extends ServerCore {
 	protected _port: number;
 	protected _host: string;
 	protected _backlog: number;
 
-	constructor(listener: Listener) {
-		super(listener);
+	constructor(listener: RequestListener) {
+		Object.defineProperty(this, '_listener', {value: listener});
+		Object.defineProperty(this, '_server', {value: http.createServer(this._listener)});
 	}
 
 	host(host: string) {
@@ -67,13 +36,46 @@ export class Server extends ServerCore {
 		return this.host(host).port(port);
 	}
 
+	start = (callback?: () => void) => {
+		this._server.listen(this._port, this._host, this._backlog, callback);
+		return this;
+	}
+
+	get server() {
+		return this._server;
+	}
+}
+
+export class Server extends ServerCore {
+	protected readonly listener: Listener;
+
+	constructor(listener: Listener) {
+		super(listener.listen);
+		this.listener = listener;
+	}
+
 	static create(listeners?: Iterable<ContextListener>) {
 		const listener = new Listener(listeners);
 		return new Server(listener);
 	}
 
-	start = (callback?: () => void) => {
-		this._server.listen(this._port, this._host, this._backlog, callback);
+	interceptor = (interceptor: (ctx: Context, error: any) => void) => {
+		this.listener.interceptor(interceptor);
+		return this;
+	}
+
+	with = (setter: (request: IncomingMessage, response: ServerResponse) => any) => {
+		this.listener.with(setter);
+		return this;
+	};
+
+	use = (nextRequestListener: ContextListener) => {
+		this.listener.register(nextRequestListener);
+		return this;
+	}
+
+	unuse = (nextRequestListener: ContextListener) => {
+		this.listener.unregister(nextRequestListener);
 		return this;
 	}
 }
