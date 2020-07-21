4a39e8f003501ff078c90dadc623a79cff6eb397
Алексей Чепугов <a.chepugov@gmail.com>
2020-07-22 01:20:56 +0300

    chore. Photo

--- source/Server/Router/index.ts
+++ source/Server/Router/index.ts
@@ -2,7 +2,7 @@ import {Router, Context} from '../../library/Server';
 import * as Photos from '../Controllers/Photos';
 import auxiliary from './auxiliary';
 
-const parseJSON = (ctx: Context) => ctx.parse('json').catch((error: any) => Promise.reject(Object.assign(error, {code: 400})));
+const parseJSON = (ctx: Context) => ctx.parse('json').catch((error: any) => Promise.reject(Object.assign(error, {reason: error.message, code: 400})));
 
 const router = new Router()
 	.on('post', '/photos/convert', parseJSON, Photos.convert)
