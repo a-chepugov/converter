2661f783b0424bb8bdb6cd085a482ec44da4f1a0
Алексей Чепугов <a.chepugov@gmail.com>
2020-07-22 13:37:21 +0300

    chore.

--- source/Server/index.ts
+++ source/Server/index.ts
@@ -1,24 +1,13 @@
-import {Server, Context} from '../library/Server';
+import {Server} from '../library/Server';
 
 import {State} from './library/State';
 import interceptor from './library/interceptor';
 import router from './Router';
 
-const PORT = 3000;
 const HOST = 'localhost';
-
-declare module "../library/Server" {
-	interface Context {
-		q: any;
-	}
-}
-
-function q(constructor: any) {
-	constructor.prototype.q = 1;
-}
+const PORT = 3000;
 
 Server
-	.context(q)
 	.create()
 	.state(State.of)
 	.use(router.listen)
