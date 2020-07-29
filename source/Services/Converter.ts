aa4ce2a1bbbb73ebad0caa422021565475df9e20
Алексей Чепугов <a.chepugov@gmail.com>
2020-07-29 23:15:13 +0300

    chore (Converter)

--- source/Services/Converter.ts
+++ source/Services/Converter.ts
@@ -60,7 +60,6 @@ export class Converter {
 						reject(Object.assign(new ConvertError(`${JSON.stringify(arguments)}. ${reason}`), {code})) :
 						resolve(outputImages.map((i) => i.fullname))
 				)
-
 			} catch (error) {
 				reject(error);
 			}
