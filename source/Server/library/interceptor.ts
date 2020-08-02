e2ee27526eb1e07a32a59126e64424d1e829a73f
Алексей Чепугов <a.chepugov@gmail.com>
2020-08-02 15:17:38 +0300

    fix (Server) interceptor

--- source/Server/library/interceptor.ts
+++ source/Server/library/interceptor.ts
@@ -4,16 +4,16 @@ import {Context} from '../../library/Server';
 
 export const interceptor = (ctx: Context, error: any) => {
 	if (!ctx.response.finished) {
-		ctx.response.statusCode = error?.code ? error.code : 500;
-		ctx.response.end(error?.reason ? error.reason : STATUS_CODES[ctx.response.statusCode]);
+		ctx.response.statusCode = error && error.code ? error.code : 500;
+		ctx.response.end(error && error.reason ? error.reason : STATUS_CODES[ctx.response.statusCode]);
 	}
 
-	if (error?.message) {
+	if (error && error.message) {
 		error.message = `[${ctx.state.id}] ${error.message}`;
 	}
 
 	console.error(error, `: ${ctx.request.url}`);
-	global.logger.error((error?.message ? error.message : error), `: ${ctx.request.url}`);
+	global.logger.error((error && error.message ? error.message : error), `: ${ctx.request.url}`);
 }
 
 export default interceptor;
