46d92f5c7e9c054c8f6c2dad460a8cb38a1c9ccc
Алексей Чепугов <a.chepugov@gmail.com>
2020-09-03 13:43:50 +0300

    chore (global) uncaughtException log

--- source/global/index.ts
+++ source/global/index.ts
@@ -12,3 +12,10 @@ process.on('unhandledRejection', (reason, promise) => {
 	global.logger.error('Unhandled Rejection at:', promise, 'reason:', reason);
 	process.exit(1);
 });
+
+process.on('uncaughtException', (error) => {
+	console.error('Uncaught Exception at:');
+	console.error(error);
+	global.logger.error('Uncaught Exception at:', error && error.message ? error.message : error);
+	process.exit(1);
+});
