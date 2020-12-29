5323ad2a80d3f4ea90b9c30b157a05a9a6001c87
Алексей Чепугов <a.chepugov@gmail.com>
2020-12-29 14:05:25 +0200

    chore(server) clientError handler

--- source/Server/index.ts
+++ source/Server/index.ts
@@ -14,3 +14,7 @@ Server
 	.interceptor(interceptor)
 	.on(HOST, PORT)
 	.start(() => console.info(`Server started on http://${HOST}:${PORT}`))
+	.server
+	.on('clientError', (error, socket) =>
+		error.code === 'ECONNRESET' || !socket.writable ? undefined : socket.end('HTTP/1.1 400 Bad Request\r\n\r\n')
+	)
