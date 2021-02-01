26dea28117429e342b3625bb8d83853e38083a3b
Алексей Чепугов <a.chepugov@gmail.com>
2021-02-01 17:50:38 +0200

    chore(ImageMagick) command example

---
+++ test/magick.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+magick ../input/0.gif -strip \
+\( +clone \
+-resize '%[fx:w>=h?800:600]x%[fx:w>=h?600:800]' \
+-rotate 90 \
+-gravity NorthEast -background none -fill '#ffffffB0' -pointsize 150 -font ../static/dixiland_mio.ttf -annotate 0x0+44-33 i \
+\( example.gif -gravity SouthWest -rotate 90 -resize 150% -geometry +0+200 \) -composite \
+-quality 70 -unsharp 0.6x0.6+1+0.05 -interlace partition -colorspace RGB \
+-write ../output/0fx.jpg +delete \
+\) \
+-resize '%[fx:w>=h?1600:1200]x%[fx:w>=h?1200:1600]>' -colorspace RGB ../output/0.jpeg
