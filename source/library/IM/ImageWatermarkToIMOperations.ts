95b350ebcea020ebad7cc99b6540b10023a77f3f
Алексей Чепугов <a.chepugov@gmail.com>
2021-02-05 18:19:52 +0200

    feature(Watermark) add Watermark Image converter

--- source/library/IM/ImageWatermarkToIMOperations.ts
+++ source/library/IM/ImageWatermarkToIMOperations.ts
@@ -1,5 +1,5 @@
-import {Operators, Option, Settings} from 'imagemagick-cli-wrapper';
-import ImageWatermark from '../../Models/ImageWatermark';
+import {Input, Operators, Option, Settings, SequenceOperators, StacksOperations, Geometry} from 'imagemagick-cli-wrapper';
+import {ImageWatermark, ImageWatermarkText, ImageWatermarkImage} from '../../Models/ImageWatermark';
 import convertAlignToGravityType from './helpers/convertAlignToGravityType';
 
 declare module "../../Models/ImageWatermark" {
@@ -8,7 +8,7 @@ declare module "../../Models/ImageWatermark" {
 	}
 }
 
-ImageWatermark.prototype.toIMOperations = function (): Option[] {
+ImageWatermarkText.prototype.toIMOperations = function (): Option[] {
 	const parameters = [];
 
 	const gravityType = convertAlignToGravityType(this.align, this.valign);
@@ -16,14 +16,34 @@ ImageWatermark.prototype.toIMOperations = function (): Option[] {
 		parameters.push(new Settings.Gravity(gravityType));
 	}
 
-	if (this.text) {
-		parameters.push(new Operators.Repage());
-		parameters.push(new Settings.Colorspace(Settings.ColorspaceType.RGB));
-		parameters.push(new Settings.Pointsize(this.text.size));
-		parameters.push(new Settings.Fill(this.text.color));
-		parameters.push(new Settings.Font(this.text.font));
-		parameters.push(new Operators.Annotate.DegreesXYTxTy(this.offset.degreesX, this.offset.degreesY, this.offset.x, this.offset.y, this.text.text))
-	}
+	parameters.push(new Settings.Pointsize(this.text.size));
+	parameters.push(new Settings.Fill(this.text.color));
+	parameters.push(new Settings.Font(this.text.font));
+	parameters.push(new Operators.Annotate.DegreesXYTxTy(this.rotate, this.rotate, this.offset.x, this.offset.y, this.text.text))
 
 	return parameters;
+};
+
+ImageWatermarkImage.prototype.toIMOperations = function (): Option[] {
+	const parameters = [];
+  parameters.push(new Input.Globbing(this.source));
+
+	const gravityType = convertAlignToGravityType(this.align, this.valign);
+	if (gravityType) {
+		parameters.push(new Settings.Gravity(gravityType));
+	}
+
+	if (this.rotate) {
+		parameters.push(new Operators.Rotate(this.rotate));
+	}
+
+	if (this.size) {
+		parameters.push(new Operators.Resize(new Geometry.Scale(this.size)));
+	}
+
+	if (this.offset) {
+		parameters.push(new Operators.Geometry(new Geometry.Offsetted(undefined, new Geometry.Offset(this.offset.x, this.offset.y))));
+	}
+
+	return [new StacksOperations.Group(parameters), new SequenceOperators.Composite()];
 };
